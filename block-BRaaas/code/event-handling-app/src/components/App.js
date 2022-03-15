22675a0ad76acd551452281ab4a4a08435f80b0c
john horo <johnhoro125@gmail.com>
2022-03-16 00:02:47 +0530

    click-event

---
+++ block-BRaaas/code/event-handling-app/src/components/App.js
@@ -0,0 +1,17 @@
+import ClickMeBtn from "./ClickMeBtn";
+import HelpBtn from "./HelpBtn";
+import PersonBtn from "./PersonBtn";
+import FruitsBtn from "./FruitsBtn";
+
+function App() {
+  return (
+    <div className="box">
+      <ClickMeBtn />
+      <HelpBtn />
+      <PersonBtn />
+      <FruitsBtn />
+    </div>
+  );
+}
+
+export default App;
