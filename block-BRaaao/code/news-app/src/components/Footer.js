876b418673ea7a20d82c11786feebd1251a22f44
john horo <johnhoro125@gmail.com>
2022-03-15 14:39:55 +0530

    article-app

---
+++ block-BRaaao/code/news-app/src/components/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <>
+      <footer className="flex item-center justify-between">
+        <small>2022 All rights reserved</small>
+        <ul className="flex justify-center">
+          <li className="mr-5">Facebook</li>
+          <li className="mr-5">Twitter</li>
+          <li className="mr-5">LinkedIn</li>
+        </ul>
+      </footer>
+    </>
+  );
+}
+export default Footer;
---
+++ block-BRaaao/code/news-app/src/components/Hero.js
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <>
+      <div className="flex hero">
+        <img src="/images/articles.jpg" alt="Article" />
+      </div>
+    </>
+  );
+}
+export default Hero;
